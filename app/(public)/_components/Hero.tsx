f145709a0a1d5d603eaee1402b4917f2651f2b99
Gilbert Owen <88517849+GilbertOwen@users.noreply.github.com>
2024-04-21 21:36:01 +0700

    Fix build error (#2)
    * feat: installed react-icons dependencies
    
    * refactor: remove unecessary style
    
    * refactor: changed the font into poppins
    
    * feat: created footer and navbar component
    
    * feat: changed the application title and description
    
    * feat: added framer-motion and gsap library for animation
    
    * refactor: changed the style of scrollbar
    
    * feat: added layout for public pages
    
    * feat: created hero component
    
    * feat: created personality and hobby component
    
    * feat: created Home public page
    
    * feat: added resource for design layout
    
    * fix: fixed build error
    
    * fix build error
    
    ---------
    
    Co-authored-by: Gilbert Owen <moptius@gmail.com>

--- app/(public)/_components/Hero.tsx
+++ app/(public)/_components/Hero.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { RiFacebookBoxFill, RiInstagramFill, RiTwitterXFill } from "react-icons/ri";
 
 export default async function Hero() {
+
+  const heroDescription = "Web Developer & Logic Enthusiast Passionate web developer, embracing challenges with logic and creativity. Thriving on problem-solving, I craft experiences that blend innovation and functionality. Let's build something extraordinary together."
   return (
     <section className="grid grid-cols-1 gap-y-6 md:grid-cols-2 min-h-screen" id="hero">
       <div className="flex-grow flex flex-col md:items-start items-center text-center md:text-left justify-center mb-4 pl-0 md:pl-10 md:order-1 order-2 md:px-0">
@@ -10,10 +12,7 @@ export default async function Hero() {
           My name is Gilbert Owen
         </h3>
         <p className="text-[#DEAC80] text-lg md:text-xl font-normal w-[90%] mt-[24px]">
-          Web Developer & Logic Enthusiast Passionate web developer, embracing
-          challenges with logic and creativity. Thriving on problem-solving, I
-          craft experiences that blend innovation and functionality. Let's build
-          something extraordinary together.
+          {heroDescription}
         </p>
         <p className="mt-[32px] text-[#B5C18E] font-medium text-lg">I would be happy if you contact me!</p>
         <div className="flex flex-row gap-x-1">
