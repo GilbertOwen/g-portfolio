825d539d0f8737d6a810978ae67ee9152113e108
Gilbert Owen <88517849+GilbertOwen@users.noreply.github.com>
2024-04-22 13:23:30 +0700

    Upgraded main page (#3)
    * feat: installed react-icons dependencies
    
    * refactor: remove unecessary style
    
    * refactor: changed the font into poppins
    
    * feat: created footer and navbar component
    
    * feat: changed the application title and description
    
    * feat: added framer-motion and gsap library for animation
    
    * refactor: changed the style of scrollbar
    
    * feat: added layout for public pages
    
    * feat: created hero component
    
    * feat: created personality and hobby component
    
    * feat: created Home public page
    
    * feat: added resource for design layout
    
    * fix: fixed build error
    
    * fix build error
    
    * feat: added resource of hobby and personality to home page
    
    * feat: added animation to hero component and section
    
    * feat: added animation to personality and hobby component
    
    ---------
    
    Co-authored-by: Gilbert Owen <moptius@gmail.com>

--- app/(public)/_components/Personality.tsx
+++ app/(public)/_components/Personality.tsx
@@ -1,20 +1,35 @@
 "use client";
 import { motion } from "framer-motion";
-import { useRef, useState } from "react";
+import gsap from "gsap";
+import { useEffect, useRef, useState } from "react";
+import { FaHiking } from "react-icons/fa";
+import { FaGamepad, FaHandshake, FaLaptopCode } from "react-icons/fa6";
+import { GiCycle, GiLoveMystery } from "react-icons/gi";
 import { ImCross } from "react-icons/im";
+import { IoBarChart } from "react-icons/io5";
 import { MdOutlineContentPasteSearch } from "react-icons/md";
+import { PiCookingPotBold } from "react-icons/pi";
+import { RiMovieFill } from "react-icons/ri";
 
-export default function Personality() {
-  const load = [1, 2, 3, 4, 5];
+type personality = {
+  name: string;
+  content: string;
+};
+type hobby = {
+  name: string;
+  content: string;
+};
+
+export default function Personality({
+  hobby,
+  personality,
+}: {
+  hobby: hobby[];
+  personality: personality[];
+}) {
   const outerModal = useRef<any>(null);
   const modal = useRef<any>(null);
-  const [isHovered, setIsHovered] = useState(false);
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
+  const [modalContent, setModalContent] = useState<personality>(personality[0]);
   const [modalOpen, setModalOpen] = useState<boolean>(false);
 
   const handleCloseModal = (e: any) => {
@@ -22,37 +37,108 @@ export default function Personality() {
       setModalOpen(false);
     }
   };
+  const handleModalOpen = (value: personality) => {
+    setModalContent(value);
+    setModalOpen(true);
+  };
+  useEffect(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".personality", // Change this to your desired trigger element
+        start: "top center", // Adjust as needed
+        end: "bottom center", // Adjust as needed
+        scrub: true, // Smooth animation when scrolling
+        toggleActions: "play restart reverse none", // Animation direction on scroll
+      },
+    });
+
+    tl.from(".personality", {
+      opacity: 0.5,
+      x: -1400,
+    }).to(".personality", {
+      opacity: 1,
+      x: 0,
+      duration: 2,
+    });
+    tl.from(".hobby", {
+      opacity: 0.5,
+      x: -1400,
+    }).to(".hobby", {
+      opacity: 1,
+      x: 0,
+      duration: 2,
+    });
+
+    return () => {
+      tl.kill(); // Kill the animation when component unmounts
+    };
+  });
+  const handleIcon = (type: string, name: string) => {
+    const theType = name.toLowerCase();
+    if (type === "personality") {
+      if (theType === "responsible") {
+        return <MdOutlineContentPasteSearch color="#F7DCB9" size={150} />;
+      } else if (theType === "credible") {
+        return <IoBarChart color="#F7DCB9" size={140} />;
+      } else if (theType === "adaptive") {
+        return <GiCycle color="#F7DCB9" size={140} />;
+      } else if (theType === "respective") {
+        return <FaHandshake color="#F7DCB9" size={140} />;
+      } else if (theType === "enthusiast") {
+        return <GiLoveMystery color="#F7DCB9" size={140} />;
+      }
+    }else if(type === 'hobby'){
+      if (theType === "hiking") {
+        return <FaHiking color="#F7DCB9" size={150} />;
+      } else if (theType === "gaming") {
+        return <FaGamepad color="#F7DCB9" size={140} />;
+      } else if (theType === "coding") {
+        return <FaLaptopCode color="#F7DCB9" size={140} />;
+      } else if (theType === "cooking") {
+        return <PiCookingPotBold color="#F7DCB9" size={140} />;
+      } else if (theType === "watching") {
+        return <RiMovieFill color="#F7DCB9" size={140} />;
+      }
+    }
+  };
   return (
-    <section id="personality" className="min-h-screen flex flex-col py-6 mt-6">
-      <div className="flex flex-col mb-4 gap-y-6">
+    <section
+      id="personality"
+      className="2xl:container 2xl:mx-auto min-h-screen flex flex-col py-6 mt-6"
+    >
+      <div className="personality flex flex-col mb-4 gap-y-6">
         <h1 className="text-[40px] md:text-5xl font-semibold text-center text-[#DEAC80]">
           Personality
         </h1>
-        <div className="grid place-content-center grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 px-4 sm:px-8 md:px-12 lg:px-24 gap-x-4 py-8 bg-[#DEAC80] text-[#F7DCB9]">
-          {load.length > 0 &&
-            load.map((i, index) => (
+        <div className="personality-container grid place-content-center grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 px-4 sm:px-8 md:px-12 lg:px-24 gap-4 py-8 bg-[#DEAC80] text-[#F7DCB9]">
+          {personality.length > 0 &&
+            personality.map((person, index) => (
               <div
                 className="flex flex-col items-center justify-center gap-y-4 cursor-pointer select-none"
                 key={index}
-                onClick={(e) => setModalOpen(true)}
+                onClick={(e) => handleModalOpen(person)}
               >
-                <MdOutlineContentPasteSearch color="#F7DCB9" size={150} />
-                <h4 className="text-2xl md:text-3xl font-semibold">Responsible</h4>
+                {handleIcon('personality' ,person.name)}
+                <h4 className="text-2xl md:text-3xl font-semibold">
+                  {person.name}
+                </h4>
               </div>
             ))}
         </div>
       </div>
-      <div className="flex flex-col gap-y-6">
-        <div className="grid place-items-center grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 px-4 sm:px-8 md:px-12 lg:px-24 py-8 gap-x-4 bg-[#B5C18E] text-[#F7dCB9] select-none">
-          {load.length > 0 &&
-            load.map((i, index) => (
+      <div className="2xl:container 2xl:mx-auto hobby flex flex-col gap-y-6">
+        <div className="hobby-container grid place-items-center grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 px-4 sm:px-8 md:px-12 lg:px-24 py-8 gap-4 bg-[#B5C18E] text-[#F7dCB9] select-none">
+          {hobby.length > 0 &&
+            hobby.map((like, index) => (
               <div
                 className="flex flex-col items-center justify-center gap-y-4 cursor-pointer"
                 key={index}
-                onClick={(e) => setModalOpen(true)}
+                onClick={(e) => handleModalOpen(like)}
               >
-                <MdOutlineContentPasteSearch color="#F7DCB9" size={150} />
-                <h4 className="text-2xl md:text-3xl font-semibold">Responsible</h4>
+                {handleIcon('hobby', like.name)}
+                <h4 className="text-2xl md:text-3xl font-semibold">
+                  {like.name}
+                </h4>
               </div>
             ))}
         </div>
@@ -65,44 +151,35 @@ export default function Personality() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          transition={{ duration:0.2 }}
+          transition={{ duration: 0.2 }}
           className="flex flex-col justify-center items-center fixed top-0 left-0 right-0 bottom-0 bg-[rgba(0,0,0,0.5)]"
           onClick={(e) => handleCloseModal(e)}
           ref={outerModal}
         >
           <motion.div
-            className="rounded-lg w-[80%] sm:w-[70%] md:w-[60%] lg:w-[50%] bg-[#F7dCB9] py-6 px-8 flex flex-col gap-y-4 relative"
+            className="rounded-lg w-[80%] sm:w-[70%] md:w-[60%] lg:w-[50%] max-h-[90%] overflow-y-auto bg-[#F7dCB9] py-6 px-8 flex flex-col gap-y-4 relative"
             ref={modal}
-            transition={{ type: "spring", delay: 0.2, x: { duration: 0.5 }, ease: 'linear', duration: 0.5 }}
+            transition={{
+              type: "spring",
+              delay: 0.2,
+              x: { duration: 0.5 },
+              ease: "linear",
+              duration: 0.5,
+            }}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ x: -300, opacity: 0 }}
           >
             <ImCross
               className="absolute top-[20px] right-[20px] cursor-pointer"
-              onMouseEnter={handleMouseEnter}
-              onMouseLeave={handleMouseLeave}
               onClick={() => setModalOpen(false)}
-              style={{ color: `${!isHovered ? "#DEAC80" : "#B99470"}` }}
+              color="#DEAC80"
               size={20}
             />
             <h1 className="text-[32px] text-[#DEAC80] font-semibold">
-              Responsible
+              {modalContent.name}
             </h1>
-            <p className="text-[#B99470]">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
-              placeat molestias dolorum quibusdam minima aliquam doloremque
-              blanditiis autem ut iusto? Et dolorem maiores nam minus nobis
-              magni accusantium animi illo, deleniti porro nemo illum iusto
-              mollitia, harum voluptatem ducimus impedit molestias. Quod quas
-              nisi dignissimos repellat velit cupiditate, voluptatibus iusto
-              cum, quisquam fuga a, facilis necessitatibus dolorum optio minus
-              sed accusamus. Rerum odit totam enim! Officia beatae repudiandae
-              iure cumque culpa, aperiam adipisci alias minima illo
-              reprehenderit reiciendis ex impedit assumenda non voluptatum
-              expedita sunt quasi quisquam commodi! Sequi architecto nisi
-              eligendi aliquid nemo ipsam ipsum molestias quos dicta explicabo!
-            </p>
+            <p className="text-[#B99470]">{modalContent.content}</p>
           </motion.div>
         </motion.div>
       )}
--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -4,10 +4,64 @@ import Hero from "./_components/Hero";
 import Personality from "./_components/Personality";
 
 export default function Home() {
+  const hobby = [
+    {
+      name: "Hiking",
+      content:
+        "Adventurous: Hiking is a thrilling adventure that brings me closer to nature. Exploring new trails, conquering challenging terrains, and immersing myself in breathtaking landscapes fuel my sense of adventure and love for the outdoors.",
+    },
+    {
+      name: "Gaming",
+      content:
+        "Immersive: Gaming is a captivating experience that transports me to different worlds. Whether I'm unraveling epic stories, strategizing in competitive battles, or connecting with friends in virtual realms, gaming immerses me in a world of excitement and endless possibilities.",
+    },
+    {
+      name: "Coding",
+      content:
+        "Innovative: Coding is a journey of innovation and problem-solving. Through coding, I create solutions, build software, and contribute to technological advancements. It's a space where creativity meets logic, allowing me to turn ideas into functional realities.",
+    },
+    {
+      name: "Cooking",
+      content:
+        "Creative: Cooking is my creative outlet where I blend flavors, experiment with recipes, and create delicious meals. From comforting classics to innovative dishes, cooking allows me to express myself artistically while sharing joy and nourishment with others.",
+    },
+    {
+      name: "Watching",
+      content:
+        "Engaging: Watching is a captivating experience that opens doors to diverse stories and perspectives. Whether I'm watching films, documentaries, or series, each viewing is an engaging journey that sparks curiosity, emotions, and new insights.",
+    },
+  ];
+  const personality = [
+    {
+      name: "Responsible",
+      content:
+        "Responsibility is a cornerstone of my daily life, guiding my actions and decisions in both personal and professional realms. I believe in owning up to my commitments, acknowledging the impact of my choices, and striving for integrity in everything I do. This means managing my time effectively, meeting deadlines, and taking accountability for any mistakes I make. In my interactions with others, I prioritize open communication, collaboration, and ethical behavior. Additionally, I recognize the importance of contributing positively to my community and environment, whether through volunteering efforts or sustainable practices. Overall, responsibility to me is a mindset that shapes how I navigate challenges, uphold values, and strive for continuous growth.",
+    },
+    {
+      name: "Credible",
+      content:
+        "Credibility is key in gaining trust and reliability in all areas of life. It's about being truthful, clear, and consistent in both words and deeds. Professionally, credibility is shown through expertise, experience, and a proven history of success. This means offering accurate information, meeting commitments, and following ethical guidelines. Credible individuals also welcome feedback and admit their limitations, showing humility and a commitment to continuous improvement. In personal relationships, credibility is established through honesty, dependability, and keeping promises. It's a quality that develops over time but is vital for building strong connections and lasting trust with others.",
+    },
+    {
+      name: "Adaptive",
+      content:
+        "The hallmark of my personality is adaptability. I possess a keen ability to navigate through various situations with flexibility, resilience, and a positive outlook. This trait is reflected in my personal and professional endeavors, where I thrive in dynamic environments and excel in the face of challenges. Adaptability is at the core of my approach to life. I embrace change as an opportunity for growth and innovation, always seeking new experiences and learning from diverse perspectives. This curiosity and openness allow me to stay agile and continuously evolve. One of my strengths is maintaining composure under pressure. I approach unexpected situations with calmness, using them as catalysts for creative problem-solving. This mindset not only helps me overcome obstacles but also inspires those around me to adopt a solution-oriented approach.",
+    },
+    {
+      name: "Respective",
+      content:
+        "Respectfulness is a fundamental aspect of my personality that guides my interactions and approach to life. I believe in treating others with dignity, empathy, and consideration, regardless of differences or circumstances. This trait is evident in my personal and professional relationships, where I prioritize mutual understanding and cooperation. Respect is the cornerstone of effective communication. I actively listen to others, value their perspectives, and express my thoughts and opinions with tact and diplomacy. This approach fosters trust and creates a positive environment where ideas can be freely exchanged and collaboration can flourish. In my professional endeavors, I demonstrate respect by acknowledging the expertise and contributions of colleagues and collaborators. I believe in the power of teamwork and recognize that each person brings unique strengths to the table. By honoring and leveraging these strengths, we can achieve greater success together.",
+    },
+    {
+      name: "Enthusiast",
+      content:
+        "Enthusiasm is a defining trait that fuels my approach to life and drives my passion for various endeavors. I possess a natural curiosity and a zest for exploration, always eager to dive into new experiences and opportunities. This enthusiasm not only energizes me but also inspires those around me. One of my strengths is my ability to maintain a positive and optimistic outlook, even in challenging situations. I believe that enthusiasm is contagious and can uplift and motivate others. This positivity creates a supportive and dynamic environment where creativity thrives, and goals are pursued with vigor. In my professional pursuits, enthusiasm fuels my commitment to excellence. I approach tasks and projects with a sense of excitement and dedication, constantly seeking ways to innovate and exceed expectations. This enthusiasm for continuous improvement drives me to learn new skills, stay updated with industry trends, and embrace challenges as opportunities for growth.",
+    },
+  ];
   return (
     <Fragment>
       <Hero></Hero>
-      <Personality></Personality>
+      <Personality personality={personality} hobby={hobby}></Personality>
     </Fragment>
   );
 }
