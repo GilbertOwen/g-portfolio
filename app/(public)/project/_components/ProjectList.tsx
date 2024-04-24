9380a1d2c827246e2713612c60c76f7f61cef47e
Gilbert Owen <moptius@gmail.com>
2024-04-24 16:14:40 +0700

    feat: added key to mapping in projectlist

--- app/(public)/project/_components/ProjectList.tsx
+++ app/(public)/project/_components/ProjectList.tsx
@@ -10,8 +10,8 @@ type project = {
 export default function ProjectList({ projects }: { projects: project[] }) {
   return (
     <div className="grid grid-cols-1 px-4 md:px-10 pb-16 gap-y-4">
-      {projects.map((project) => (
-        <div className="grid grid-cols-1 relative md:grid-cols-2">
+      {projects.map((project, index) => (
+        <div className="grid grid-cols-1 relative md:grid-cols-2" key={index}>
           <div className="md:static w-full absolute bg-[rgba(0,0,0,0.3)] md:bg-[#DEAC80] md:text-left text-center top-0 md:items-start items-center flex flex-col pl-4 pr-4 md:pr-0 md:pl-10 py-4 md:py-8 h-full select-none">
             <h1 className="text-2xl sm:text-4xl md:text-5xl font-semibold text-[#F7DCB9] select-none mb-2 sm:mb-4 md:mb-8">
               {project.name}
