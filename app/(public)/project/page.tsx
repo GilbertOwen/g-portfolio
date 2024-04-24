bc5cbf4a9553e15962b6db8d3ae5589d057a134e
Gilbert Owen <moptius@gmail.com>
2024-04-24 16:13:57 +0700

    feat: created project public page

---
+++ app/(public)/project/page.tsx
@@ -0,0 +1,30 @@
+import { Fragment } from "react";
+import CarouselProject from "./_components/CarouselProject";
+import ProjectList from "./_components/ProjectList";
+import image1 from "@/public/assets/laptop-analyze.jpg";
+import image2 from "@/public/assets/laptop-program.jpg";
+
+export default async function ProjectPage() {
+  const projects = [
+    {
+      name: "To-do List Project",
+      date: '26-03-2000',
+      description: 'This project is me itself. From this website, you will know about my personality, hobby, and etc. The project is this website itself though',
+      image: image1.src,
+    },
+    {
+      name: "Community Project",
+      date: '26-03-2024',
+      description: 'This project is me itself. From this website, you will know about my personality, hobby, and etc. The project is this website itself though',
+      image: image2.src,
+    },
+  ];
+  return (
+    <Fragment>
+      <div className="2xl:container 2xl:mx-auto min-h-screen flex flex-col pt-24 md:pt-28 pb-14">
+        <CarouselProject></CarouselProject>
+      </div>
+      <ProjectList projects={projects}></ProjectList>
+    </Fragment>
+  );
+}
