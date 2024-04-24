6438274204425bac1a2ee011eb03cf06d6d9e145
Gilbert Owen <moptius@gmail.com>
2024-04-24 10:41:31 +0700

    feat: added animation to studied content

--- app/(public)/more/_components/Studied.tsx
+++ app/(public)/more/_components/Studied.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { motion } from "framer-motion";
 import { useState } from "react";
 import { FaCaretLeft, FaCaretRight, FaCss3Alt, FaHtml5 } from "react-icons/fa6";
 
@@ -7,7 +8,8 @@ export default function Studied({
 }: {
   lessons: { name: string; icon: JSX.Element; description: string }[];
 }) {
-  const studiedDescription = "Along my journey to go deep into technology world, i've studied things below :"
+  const studiedDescription =
+    "Along my journey to go deep into technology world, i've studied things below :";
   const [order, setOrder] = useState<number>(0);
   const handleButton = (type: string) => {
     if (type === "prev") {
@@ -25,17 +27,40 @@ export default function Studied({
     }
   };
   return (
-    <section
+    <motion.section
       id="studied"
-      className="2xl:container 2xl:mx-auto min-h-screen flex flex-col pt-28 px-4 sm:px-12 md:px-24 lg:px-40 xl:px-50"
+      className="studied 2xl:container 2xl:mx-auto min-h-screen flex flex-col pt-28 px-4 sm:px-12 md:px-24 lg:px-40 xl:px-50"
     >
-      <h1 className="text-[40px] md:text-5xl font-semibold text-center select-none text-[#B99470] mb-6">
+      <motion.h1
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0, transition: { duration: 0.6 } }}
+        exit={{ opacity: 0, y: 100 }}
+        className="text-[40px] md:text-5xl font-semibold text-center select-none text-[#B99470] mb-6"
+      >
         Studied
-      </h1>
-      <p className="text-center font-regular text-xl xl:text-2xl select-none text-[#B99470] mb-8">
+      </motion.h1>
+      <motion.p
+        initial={{ opacity: 0, y: 100 }}
+        animate={{
+          opacity: 1,
+          y: 0,
+          transition: { duration: 0.6, delay: 0.3 },
+        }}
+        exit={{ opacity: 0, y: 100 }}
+        className="text-center font-regular text-xl xl:text-2xl select-none text-[#B99470] mb-8"
+      >
         {studiedDescription}
-      </p>
-      <div className="flex flex-row items-center gap-x-1 md:gap-x-4 w-fit mx-auto mb-4">
+      </motion.p>
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{
+          opacity: 1,
+          y: 0,
+          transition: { duration: 0.6, delay: 0.6 },
+        }}
+        exit={{ opacity: 0, y: 100 }}
+        className="flex flex-row items-center gap-x-1 md:gap-x-4 w-fit mx-auto mb-4"
+      >
         <FaCaretLeft
           color="#B5C18E"
           size={40}
@@ -51,15 +76,33 @@ export default function Studied({
           className="cursor-pointer"
           onClick={() => handleButton("next")}
         />
-      </div>
+      </motion.div>
       <div className="flex flex-col text-center gap-y-2 mx-auto">
-        <h2 className="text-[#C8A682] text-2xl font-semibold select-none">
+        <motion.h2
+          initial={{ opacity: 0, y: 100 }}
+          animate={{
+            opacity: 1,
+            y: 0,
+            transition: { duration: 0.6, delay: 1.2 },
+          }}
+          exit={{ opacity: 0, y: 100 }}
+          className="text-[#C8A682] text-2xl font-semibold select-none"
+        >
           {lessons[order]?.name}
-        </h2>
-        <p className="text-base md:text-lg text-[#C8A682] font-refular select-none">
+        </motion.h2>
+        <motion.p
+          initial={{ opacity: 0, y: 100 }}
+          animate={{
+            opacity: 1,
+            y: 0,
+            transition: { duration: 0.6, delay: 1.5 },
+          }}
+          exit={{ opacity: 0, y: 100 }}
+          className="text-base md:text-lg text-[#C8A682] font-refular select-none"
+        >
           {lessons[order]?.description}
-        </p>
+        </motion.p>
       </div>
-    </section>
+    </motion.section>
   );
 }
