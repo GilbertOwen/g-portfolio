29855a6ba426c8f132ebbfd012151da1967718e7
Gilbert Owen <moptius@gmail.com>
2024-04-26 11:02:35 +0700

    feat: added animations to project detail page

--- app/(public)/project/[slug]/page.tsx
+++ app/(public)/project/[slug]/page.tsx
@@ -1,37 +1,126 @@
+"use client";
 import { Fragment } from "react";
 import image1 from "@/public/assets/laptop-program.jpg";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
-export default async function ProjectDetailPage() {
+export default function ProjectDetailPage() {
   const project = {
     description:
       "This project is a reflection of myself, encapsulating my personality, interests, and experiences. Through this website, visitors will gain insights into who I am, my passions, and the work I've done. Every aspect of this portfolio is meticulously crafted to showcase not just my skills but also my unique identity. From the design elements to the content, each detail is a deliberate choice that represents me authentically. This website serves as both a professional showcase and a personal statement, allowing others to connect with me on a deeper level and understand what drives me creatively and professionally.",
     name: "Portfolio Project",
-    createdAt: '19-07-2000'
-    };
+    createdAt: "19-07-2000",
+  };
   return (
     <Fragment>
       <div className="w-full flex flex-col pt-24 md:pt-28 pb-12 overflow-hidden relative gap-y-10">
-        <img
+        <motion.img
+          initial={{
+            opacity: 0,
+            x: -1400,
+          }}
+          animate={{
+            opacity: 1,
+            x: 0,
+            transition: {
+              duration: 1,
+              ease: "linear",
+            },
+          }}
           src={image1.src}
-          className="block select-none object-cover object-center rounded-none h-[570px] w-full"
+          className="block select-none object-cover object-center rounded-none h-[320px] md:h-[500px] w-full"
           alt=""
         />
-        <div className="2xl:container 2xl:mx-auto grid grid-cols-1 px-4 md:px-20 pb-16">
+        <div className="2xl:container 2xl:mx-auto grid grid-cols-1 px-4 md:px-20 pb-12">
           <div className="content-header w-fit mx-auto mb-10">
-            <h1 className="text-[48px] uppercase font-semibold text-[#DEAC80] px-4 ">
+            <motion.h1
+              initial={{
+                opacity: 0,
+                x: -200,
+              }}
+              animate={{
+                opacity: 1,
+                x: 0,
+                transition: {
+                  duration: 0.3,
+                  delay: 1,
+                  ease: "linear",
+                },
+              }}
+              className="text-[36px] md:text-[48px] uppercase text-center font-semibold text-[#DEAC80] px-2 "
+            >
               {project.name}
-            </h1>
-            <hr className="h-1 bg-[#B5C18E] border-[#B5C18E] w-full mx-auto" />
-            <p className="text-[#DEAC80] text-[16px] font-medium text-center mt-2">
+            </motion.h1>
+            <motion.hr
+              initial={{
+                opacity: 0,
+                x: -200,
+              }}
+              animate={{
+                opacity: 1,
+                x: 0,
+                transition: {
+                  duration: 0.3,
+                  delay: 1.3,
+                  ease: "linear",
+                },
+              }}
+              className="h-1 bg-[#B5C18E] border-[#B5C18E] w-full mx-auto"
+            />
+            <motion.p
+              initial={{
+                opacity: 0,
+                x: -200,
+              }}
+              animate={{
+                opacity: 1,
+                x: 0,
+                transition: {
+                  duration: 0.3,
+                  delay: 1.6,
+                  ease: "linear",
+                },
+              }}
+              className="text-[#DEAC80] text-[16px] font-medium text-center mt-1"
+            >
               {project.createdAt}
-            </p>
+            </motion.p>
           </div>
-          <p className="text-[32px] font-medium text-[#DEAC80] text-center">
+          <motion.p
+            initial={{
+              opacity: 0,
+              x: -200,
+            }}
+            animate={{
+              opacity: 1,
+              x: 0,
+              transition: {
+                duration: 0.3,
+                delay: 1.9,
+                ease: "linear",
+              },
+            }}
+            className="text-[16px] md:text-[24px] w-full md:w-[80%] lg:w-[70%] mx-auto font-medium text-[#DEAC80] text-center"
+          >
             {project.description}
-          </p>
+          </motion.p>
         </div>
-        <div className="px-20 place-content-center grid grid-cols-1 md:grid-cols-2 gap-y-4 gap-x-4 md:gap-x-10">
+        <motion.div
+          initial={{
+            opacity: 0,
+            x: -200,
+          }}
+          animate={{
+            opacity: 1,
+            x: 0,
+            transition: {
+              duration: 0.3,
+              delay: 2.2,
+              ease: "linear",
+            },
+          }}
+          className="2xl:container 2xl:mx-auto mx-auto w-[90%] md:w-[80%] lg:w-[70%] place-content-center grid grid-cols-1 md:grid-cols-2 gap-y-4 gap-x-4 md:gap-x-10"
+        >
           <Link
             target="_blank"
             href={"https://github.com/GilbertOwen/g-portfolio"}
@@ -45,7 +134,7 @@ export default async function ProjectDetailPage() {
           >
             Vercel
           </Link>
-        </div>
+        </motion.div>
       </div>
     </Fragment>
   );
